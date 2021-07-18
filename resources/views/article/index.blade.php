dc5a85b8bfc7a673ce067000fd8748b606a0b50d
SeyH0409 <199894sei@gmail.com>
2021-07-19 00:03:20 +0900

    ルーティング設定

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>anitalk</title>
+</head>
+<body>
+  <h1>アニトーク</h1>
+</body>
+</html>
